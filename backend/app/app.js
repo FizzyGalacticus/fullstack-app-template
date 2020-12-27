bb08dd88d558f4cb330f09aeecd962115ede5870
Dustin Dodson <dustinleedodson@gmail.com>
2020-12-26 23:22:00 -0800

    Setup infra + BE (#1)
    * Sets up ESLint for entire project
    
    * Sets up husky git hooks
    
    * Sets up database migrations + Fastify server/routes/controllers
    
    * Sets up docker-compose file + related package.json scripts

---
+++ backend/app/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+
+const fastify = require('fastify')();
+
+const logger = console;
+
+const routesPath = path.join(__dirname, 'routes');
+
+const routeVersionPaths = fs.readdirSync(routesPath);
+
+routeVersionPaths.forEach(routeVersion => {
+    const versionRoutePath = path.join(routesPath, routeVersion);
+
+    const versionRoutes = fs.readdirSync(versionRoutePath);
+
+    versionRoutes.forEach(route => {
+        const routePath = path.join(versionRoutePath, route);
+
+        const routes = require(routePath);
+
+        routes.forEach(r => fastify.route(r));
+    });
+});
+
+fastify.listen(3000, '0.0.0.0', (err, address) => {
+    if (err) {
+        throw err;
+    }
+
+    logger.log(`Listening on ${address}`);
+});
---
+++ precommit.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const simpleGit = require('simple-git');
+const { ESLint } = require('eslint');
+
+const git = simpleGit();
+
+const main = async () => {
+    const eslint = new ESLint({ fix: true });
+
+    const gitStatus = await git.status();
+
+    const staged = gitStatus.files.reduce((acc, file) => {
+        if (file.index !== 'D') {
+            acc.push(file.path);
+        }
+
+        return acc;
+    }, []);
+
+    let lintResults = await eslint.lintFiles(staged);
+
+    await ESLint.outputFixes(lintResults);
+
+    lintResults = await eslint.lintFiles(staged);
+
+    const errors = lintResults.reduce((acc, r) => {
+        if (r.filePath.endsWith('.js') && r.errorCount) {
+            acc.push(...r.messages.map(m => JSON.stringify(m)));
+        }
+
+        return acc;
+    }, []);
+
+    if (errors.length) {
+        throw new Error(errors.join('\n'));
+    }
+
+    return git.raw(['add', ...staged]);
+};
+
+main()
+    .then(() => process.exit(0))
+    .catch(e => {
+        // eslint-disable-next-line no-console
+        console.error(e);
+
+        process.exit(1);
+    });
